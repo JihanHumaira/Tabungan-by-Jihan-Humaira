d6d0712f3b4d1b186b66e8eea2321eccbbae75ad
Jihan Humaira lenovo <lalahumaira00@gmail.com>
2025-11-27 06:56:14 +0700

    Update kalkulator

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,54 @@
-def tambah(a, b):
-    return a+b
-def kali(a, b):
-    return a*b
-def bagi(a, b):
-    return a/b
-def kurang(a, b):
-    return a-b
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(add(num1, num2))
+
+        elif choice == '2':
+            print(subtract(num1, num2))
+
+        elif choice == '3':
+            print(multiply(num1, num2))
+
+        elif choice == '4':
+            print(divide(num1, num2))
+    
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "yes":
+          continue  
+        elif next_calculation == "no":
+          break
+        else:
+          print("Invalid Input")
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-import kalkulator
+def tambah(a,b):
+    return a+b
+def kurang(a,b):
+    return a-b
+def kali(a,b):
+    return(a*b)
+def bagi(a,b):
+    return(a/b)
 
-hasil_bagi= kalkulator.bagi(8, 2)
-print(hasil_bagi)
-print(kalkulator.kurang(9,2))
+print("pilih operasi:")
+print("1. Tambah")
+print("2. Kurang")
+print("3. Kali")
+print("4. Bagi")
+choice=('1/2/3/4??'(input(choice)))
+    if choice=="1":
+    print(tambah(num1,num2))
+
+
+num1=float(input(num1))
+num2=float(input(num2))
+
+
